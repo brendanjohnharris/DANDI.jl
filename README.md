2263de14b973bd8729a4fdba0f237db5e7ba2cfa
brendanjohnharris <brendanjohnharris@gmail.com>
2023-03-20 18:09:48 +1100

    Easy little package

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 
 [![Build Status](https://github.com/brendanjohnharris/DANDI.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/brendanjohnharris/DANDI.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/brendanjohnharris/DANDI.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/brendanjohnharris/DANDI.jl)
+
+This package wraps functions from the [DANDI](https://dandiarchive.org/) [python](https://github.com/dandi/dandi-cli) API.
+
+Functions and classes from the python API can be accessed with:
+```julia
+using DANDI
+DANDI.<module>.<function>
+```
+where valid values for `<module>` and `<function>` generally follow the documentation for the python package, available [here](https://dandi.readthedocs.io/en/latest/).
+
+Additional functions have been added for convenience, such as:
+```julia
+dandiurl(dandiset_id::String, filepath::String; version::{String})
+```
+which can be used to grab the url of a dandiset from the dandiset id and the filepath of the file within the dandiset.
--- src/DANDI.jl
+++ src/DANDI.jl
@@ -1,5 +1,42 @@
 module DANDI
+using PythonCall
+using CondaPkg
 
-# Write your package code here.
+# Try and export as much of the python dandi interface as possible
+basemodules = [:pynwb, :dandi, :h5py]
+modules = [:dandiapi, :dandiarchive, :delete, :download, :move, :organize, :upload, :validate, :files, :misctypes, :consts, :utils, :support]
+# apiclasses = [
+[Meta.parse("const $m = PythonCall.pynew()") for m in basemodules] .|> eval
+[Meta.parse("const $m = PythonCall.pynew()") for m in modules] .|> eval
+
+function __init__()
+    [Meta.parse("PythonCall.pycopy!($m, pyimport(\"$m\"))") for m in basemodules] .|> eval
+    [Meta.parse("PythonCall.pycopy!($m, pyimport(\"dandi.$m\"))") for m in modules] .|> eval
+end
+
+
+
+"""
+    dandiurl(dandiset_id::String="000006", filepath::String="sub-anm372795/sub-anm372795_ses-20170718.nwb"; version::Union{Nothing, String}=nothing) -> String
+
+Returns the S3 URL of a file in a DANDI dataset.
+
+## Arguments
+- `dandiset_id`: The ID of the DANDI dataset.
+- `filepath`: The path of the file within the DANDI dataset.
+- `version`: The version of the file. If nothing, the latest version is used.
+"""
+function dandiurl(dandiset_id="000006", filepath="sub-anm372795/sub-anm372795_ses-20170718.nwb"; version=nothing)
+    client = dandiapi.DandiAPIClient()
+    if isnothing(version)
+        asset = client.get_dandiset(dandiset_id).get_asset_by_path(filepath)
+    else
+        asset = client.get_dandiset(dandiset_id).get_asset_by_path(filepath, version)
+    end
+    s3_url = asset.get_content_url(follow_redirects=1, strip_query=true)
+    return s3_url
+end
+
+
 
 end
